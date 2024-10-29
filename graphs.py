7dac461a6bf669eaf012519b6224676da64e2c97
JTS-Dawnstar <87510267+JTS-Dawnstar@users.noreply.github.com>
2024-10-28 19:45:12 -0700

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -46,14 +46,14 @@ class _data_obj:
 data__object = _data_obj()"""
 
 redraw_string = """
-preds = [data__object.prediction(known_data, value) for value in PRED_RANGE]
+preds = [data__object.prediction(known_data, value) for value in list(range(len(_data)))]
 
 # create a new plot with default tools, using figure
 p = figure(width=400, height=400)
 
 # add a circle renderer with x and y coordinates, size, color, and alpha
 p.scatter(list(KNOWN_RANGE), known_data, size=3, line_color="green", fill_color="green", fill_alpha=0.5)
-p.line(list(range(len(_data))), known_data, line_width = 2, color = "red")
+p.line(list(range(len(_data))), preds, line_width = 2, color = "red")
 p.vstrip(x0 = [KNOWN_RANGE.start], x1 = [KNOWN_RANGE.stop], fill_color = "green", fill_alpha = 0.2)
 p.vstrip(x0 = [PRED_RANGE.start], x1 = [PRED_RANGE.stop], fill_color = "red", fill_alpha = 0.2)
 p_json = json.dumps(json_item(p, "myplot"))
